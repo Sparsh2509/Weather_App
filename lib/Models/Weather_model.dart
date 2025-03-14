b69994222befd3ae67862fe3a3c08ec92591d8c8
Sparsh2509 <sparshgupta250904@gmail.com>
2025-03-14 23:18:53 +0530

    Weather model created

---
+++ lib/Models/Weather_model.dart
@@ -0,0 +1,14 @@
+class worker {
+  String location;
+  String temp;
+  String humidity;
+  String airspeed;
+  String description;
+
+  worker(
+      {required this.location,
+      required this.temp,
+      required this.humidity,
+      required this.airspeed,
+      required this.description});
+}
